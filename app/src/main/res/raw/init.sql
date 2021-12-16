a3a30acd932fad59c1f749f78cac200698ce71f4
uygarkarlalek <uy.kara@gmail.com>
2021-12-16 21:02:11 +0100

    Changed sql

--- app/src/main/res/raw/init.sql
+++ app/src/main/res/raw/init.sql
@@ -1,13 +1,11 @@
 CREATE TABLE `user` (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,    username VARCHAR (45) UNIQUE                          NOT NULL,    password VARCHAR (45) NOT NULL, points INT DEFAULT (0));
 CREATE TABLE category(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR (45) UNIQUE NOT NULL);
 CREATE TABLE question ( id INTEGER PRIMARY KEY NOT NULL, value VARCHAR (45) NOT NULL, category INTEGER NOT NULL, wright INTEGER NOT NULL, response1 VARCHAR (45) NOT NULL, response2 VARCHAR (45) NOT NULL, response3 VARCHAR (45) NOT NULL, response4 VARCHAR (45) NOT NULL, additional_data VARCHAR (45), win_points INTEGER DEFAULT (0), FOREIGN KEY(category) REFERENCES category(id));
-
 INSERT INTO category values(1, "Film & Serie");
 INSERT INTO category values(2, "Cultura generale");
 INSERT INTO category values(3, "Musica");
 INSERT INTO category values(4, "Programmazione");
 INSERT INTO category values(5, "Algoritmi");
-
 INSERT INTO question values(null, 4, 3, "Qual è il concetto, in programmazione OOP, che permette di comporre le classi in una gerarchia?", "Polimorfismo", "Type-Inference", "Ereditarietà", "Covarianza", null, 1);
 INSERT INTO question values(null, 4, 3, "Quale funzione, in kotlin, ti permette di creare una mappa mutabile con oggetti stringa?", "mapOf<String>()", "mutableMapOf<String>()", "Map.of<String>()", "CollectionUtils.mapOf<String>()", null, 3);
 INSERT INTO question values(null, 4, 4, "Qual è il framework di Android che permette il supporto delle sue librerie legacy?", "Spring", "Android Studio", "androidx", "Android Jetpack", null, 4);
